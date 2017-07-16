941fc058c831685cac562cbaf4fbe0f2b882f474
yongchenPan <83202715@qq.com>
2017-07-16 12:32:23 +0800

    新增的支付模块分支--做第二次的修改

--- src/com/pyc/test/Test.java
+++ src/com/pyc/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("潘泳辰测试一下，git代码提交");
 		System.out.println("新增的支付模块分支--有修改");
+		System.out.println("新增的支付模块分支--做第二次的修改");
 	}
 }
