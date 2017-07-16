1bb3d95af84dcc9aa4564e3ff42eece52f0bca5d
yongchenPan <83202715@qq.com>
2017-07-16 16:58:59 +0800

    支付业务逻辑代码更新 update

--- src/com/pyc/test/AddBranch.java
+++ src/com/pyc/test/AddBranch.java
@@ -6,6 +6,6 @@ package com.pyc.test;
  */
 public class AddBranch {
 	public static void main(String[] args) {
-		System.out.println("支付模块分支业务逻辑代码更新---第一次 update update");
+		System.out.println("支付模块分支业务逻辑代码更新---第一次 update");
 	}
 }
